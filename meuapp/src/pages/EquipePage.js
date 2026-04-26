731a27d035b95904485ee581a4ae92a0a1839f28
NekQH <felipe.urbanek@pucpr.edu.br>
2026-04-26 16:47:17 -0300

    Alteração de README | Equipe

--- meuapp/src/pages/EquipePage.js
+++ meuapp/src/pages/EquipePage.js
@@ -8,9 +8,9 @@ export default function EquipePage() {
 
       <div className="grid-cards">
         <Equipe nome="Felipe" cargo="Desenvolvedor" idade={20} github="https://github.com/NekQH" linkedin="https://www.linkedin.com/in/felipeurbanekg/" />
-        <Equipe nome="Enzo" cargo="Desenvolvedor" idade={20} github="https://github.com" linkedin="https://linkedin.com" />
-        <Equipe nome="Davi" cargo="Desenvolvedor" idade={19} github="https://github.com" linkedin="https://linkedin.com" />
-        <Equipe nome="Carlos" cargo="Desenvolvedor" idade={20} github="https://github.com" linkedin="https://linkedin.com" />
+        <Equipe nome="Enzo" cargo="Desenvolvedor" idade={20} github="https://github.com/enzobortolozzoborges" linkedin="https://www.linkedin.com/in/enzo-bortolozzo-borges-5912642b6/" />
+        <Equipe nome="Carlos" cargo="Desenvolvedor" idade={20} github="https://github.com/carlosewmartins" linkedin="https://www.linkedin.com/in/carlosewmartins/" />
+        <Equipe nome="Davi" cargo="Desenvolvedor" idade={19} github="https://github.com/Fushyki" linkedin="https://linkedin.com" />
       </div>
     </div>
   );
