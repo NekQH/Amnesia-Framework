23481cb1eac95665589c2d7eb7bc5b8ba4e313a9
NekQH <felipe.urbanek@pucpr.edu.br>
2026-06-08 21:27:28 -0300

    Hambuguer - Navegacao

--- meuapp/src/components/Navbar.js
+++ meuapp/src/components/Navbar.js
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, Menu, X } from "lucide-react";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <Link to="/" className="nav-logo">
+        <Link to="/" className="nav-logo" onClick={closeMenu}>
           <LayoutDashboard className="nav-logo-icon" />
           <span>Amnesia</span>
         </Link>
-        <div className="nav-links">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/equipe" className="nav-link">Equipe</Link>
-          <Link to="/contadores" className="nav-link">Contadores</Link>
-          <Link to="/cadastro" className="nav-link">Cadastro</Link>
-          <Link to="/filmes" className="nav-link">Filmes</Link>
+        
+        <div className="mobile-menu-icon" onClick={toggleMenu}>
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </div>
+
+        <div className={`nav-links ${isOpen ? "active" : ""}`}>
+          <Link to="/" className="nav-link" onClick={closeMenu}>Home</Link>
+          <Link to="/equipe" className="nav-link" onClick={closeMenu}>Equipe</Link>
+          <Link to="/contadores" className="nav-link" onClick={closeMenu}>Contadores</Link>
+          <Link to="/cadastro" className="nav-link" onClick={closeMenu}>Cadastro</Link>
+          <Link to="/filmes" className="nav-link" onClick={closeMenu}>Filmes</Link>
         </div>
       </div>
     </nav>
